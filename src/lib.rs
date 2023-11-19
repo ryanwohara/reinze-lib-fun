7e2ed89e002e978fe5f5250cf0e805228fbea2af
Ryan William O'Hara <ryan@ryanwohara.com>
2023-11-19 15:42:09 -0700

    chore: dylib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod calculator;
+
+extern crate common;
+
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
+
+#[no_mangle]
+pub extern "C" fn exported(
+    cmd: *const c_char,
+    raw_query: *const c_char,
+    raw_author: *const c_char,
+) -> *mut c_char {
+    let command = to_str_or_default(cmd);
+    let query = to_str_or_default(raw_query);
+    let _author = to_str_or_default(raw_author);
+
+    let result = match command.as_str() {
+        "calc" | "calculate" | "calculator" => calculator::calculate(query.as_str()),
+        "help" => Ok(vec!["calc".to_string()]),
+        "" => Ok("calc"
+            .split("\n")
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>()),
+        _ => Ok(vec![]),
+    };
+
+    let output = result.unwrap_or_default();
+
+    CString::new(output.join("\n")).unwrap().into_raw()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn to_str_or_default(ptr: *const c_char) -> String {
+    let cstr = unsafe { CStr::from_ptr(ptr) };
+    cstr.to_str().unwrap_or_default().to_owned()
 }
