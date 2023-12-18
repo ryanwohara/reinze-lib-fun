b8e37fede000a10a1f7a6619845f2847140dc8e2
Zac Edmiston <zac.edmiston91@gmail.com>
2023-12-17 19:29:55 -0600

    feat: cameo commands

---
+++ src/cameo.rs
@@ -0,0 +1,7 @@
+pub fn zac() -> Result<Vec<String>, String> {
+    Ok(vec!["<Ss2trunks> !nick Ss2truns_23 <Ss2trunks> Hmm <Ss2trunks> Why don't it work? <---- LOL WHAT A NOOB".to_string()])
+}
+
+pub fn dra9() -> Result<Vec<String>, String> {
+    Ok(vec!["Dra9 > Sababa".to_string()])
+}
