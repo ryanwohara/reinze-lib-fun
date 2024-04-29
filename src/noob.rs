72266023e73e01b81821016eaaedb95031661ed8
Ryan William O'Hara <ryan@ryanwohara.com>
2024-04-28 18:09:12 -0600

    fix: Random matching

--- src/noob.rs
+++ src/noob.rs
@@ -10,9 +10,8 @@ pub fn noob(query: String, mut author: String) -> Result<Vec<String>, String> {
 
     let mut r = rand::thread_rng();
     let output = match r.gen_range(0..=1) {
-        i32::MIN..=-1_i32 => "is",
-        2_i32..=i32::MAX => "is not",
-        _ => "is not",
+        0 => "is not",
+        _ => "is",
     };
 
     Ok(vec![format!("{author} {output} a silly noob!")])
