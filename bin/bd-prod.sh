7b88844764fea36cbd514312611afea86750aed8
Ryan William O'Hara <ryan@ryanwohara.com>
2025-10-01 12:54:51 -0600

    fix(ci): Multiplatform and cargo update

--- bin/bd-prod.sh
+++ bin/bd-prod.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 # bd = build & deploy
 
-cargo build --release && rm -rf ../rust-reinze/plugins/libreinze_lib_fun_*.so && cp target/release/libreinze_lib_fun.so ../rust-reinze/plugins/libreinze_lib_fun_$(date "+%Y%m%dT%H%M%S").so
+[[ $(uname -s) == "Darwin" ]] && export extension=.dylib || export extension=.so
+
+cargo update && \
+  cargo build --release && \
+  rm -rf ../rust-reinze/plugins/libreinze_lib_fun_* && \
+  cp "target/release/libreinze_lib_fun${extension}" "../rust-reinze/plugins/libreinze_lib_fun_$(date "+%Y%m%dT%H%M%S")${extension}"
