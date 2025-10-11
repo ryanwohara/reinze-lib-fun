f901185e9d0ce921db750c70b79c58115a1bcb1d
Ryan William O'Hara <ryan@ryanwohara.com>
2025-10-11 09:18:17 -0600

    fix(ci): mv not cp

--- bin/bd-prod.sh
+++ bin/bd-prod.sh
@@ -6,4 +6,4 @@
 cargo update && \
   cargo build --release && \
   rm -rf ../rust-reinze/plugins/libreinze_lib_fun_* && \
-  cp "target/release/libreinze_lib_fun${extension}" "../rust-reinze/plugins/libreinze_lib_fun_$(date "+%Y%m%dT%H%M%S")${extension}"
+  mv "target/release/libreinze_lib_fun${extension}" "../rust-reinze/plugins/libreinze_lib_fun_$(date "+%Y%m%dT%H%M%S")${extension}"
